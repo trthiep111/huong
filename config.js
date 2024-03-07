ebb1182bfa30cf042af97d720c386d41b42ca4af
trthiep111 <162547056+trthiep111@users.noreply.github.com>
2024-03-07 13:38:52 +0700

    huong

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Em';
+const giftUrl = '';
+const eventName = 'Chúc Mừng 8-3';
+const titleCard = 'Tặng ai đó :v';
+const contentCard = 'Chúc em bé của anh 8-3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+const giftImage = 'ny.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/jpg;base64, " + base64;
